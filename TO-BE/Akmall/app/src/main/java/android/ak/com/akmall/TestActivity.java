9ba51c0ca9ee29f7a833033c4a612a4499f9abe8
gkduduu <break200@break200s-MacBook-Pro.local>
2016-10-27 14:21:03 +0900

    Grade setting

--- TO-BE/Akmall/app/src/main/java/android/ak/com/akmall/TestActivity.java
+++ TO-BE/Akmall/app/src/main/java/android/ak/com/akmall/TestActivity.java
@@ -1,11 +1,24 @@
 package android.ak.com.akmall;
 
 import android.app.Activity;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_test)
 public class TestActivity extends Activity {
 
+    @ViewById
+    VideoView TEST_VIDEO;
 
+    @AfterViews
+    private void afterView(){
+        TEST_VIDEO.setVideoPath("url");
+        final MediaController mediaController =
+                new MediaController(this);
+        TEST_VIDEO.setMediaController(mediaController);
+    }
 }
